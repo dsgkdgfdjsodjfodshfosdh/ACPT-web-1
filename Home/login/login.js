44e8dfb933948156bb4a49e45ef8a09abb1932c0
AmandaLakmal <lakmalamanda@gmail.com>
2026-02-24 14:08:13 +0530

    df

--- Home/login/login.js
+++ Home/login/login.js
@@ -1,13 +1,13 @@
 function login() {
-
     var correctUsername = "amanda";
     var correctPassword = "1234";
 
     var enteredUsername = document.getElementById("username").value;
     var enteredPassword = document.getElementById("password").value;
 
-    if (enteredUsername === correctUsername && enteredPassword === correctPassword) {
-        window.location.href = "home.html";
+    if (enteredUsername == correctUsername && enteredPassword == correctPassword) {
+
+        window.location.href = "../index.html";
     } else {
         document.getElementById("message").innerText = "Invalid username or password";
     }
