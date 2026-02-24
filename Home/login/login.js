040de42b229ac4aaabc0fc5ad5460828282c3626
AmandaLakmal <lakmalamanda@gmail.com>
2026-02-24 13:35:12 +0530

    login.js created with hardcode email and  pass

---
+++ Home/login/login.js
@@ -0,0 +1,14 @@
+function login() {
+
+    var correctUsername = "amanda";
+    var correctPassword = "1234";
+
+    var enteredUsername = document.getElementById("username").value;
+    var enteredPassword = document.getElementById("password").value;
+
+    if (enteredUsername === correctUsername && enteredPassword === correctPassword) {
+        window.location.href = "home.html";
+    } else {
+        document.getElementById("message").innerText = "Invalid username or password";
+    }
+}
